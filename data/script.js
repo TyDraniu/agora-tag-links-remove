2517709c5229c1ef66d91518314f09503b187f19
Krzysztof Głębowicz <krzysztof.glebowicz@it-pkp.pl>
2015-03-24 11:22:56 +0100

    jpm version

---
+++ data/script.js
@@ -0,0 +1,16 @@
+﻿/*
+ * Test page: http://lublin.gazeta.pl/lublin/1,48724,14504860,Test.html
+ */
+ 
+var anchors = document.getElementsByClassName("external_link");
+var n = anchors.length;
+//console.log("Agora Bad Links " + n);
+try {
+	for(i = 0; i < n; i++) {
+        //console.log(anchors[0].outerHTML);
+		anchors[0].outerHTML = anchors[0].innerHTML;
+    } 
+} 
+catch(e) {  
+        console.exception(e.message);
+    }
---
+++ index.js
@@ -0,0 +1,11 @@
+// Import the page-mod API
+var pageMod = require("sdk/page-mod");
+// js as separate files supplied in "data" directory. 
+
+var data = require("sdk/self").data;
+
+
+pageMod.PageMod({
+    include: ["*.sport.pl", "*.gazeta.pl", "*.gazeta.biz", "*.tokfm.pl", "*.gazetapraca.pl", "*.moto.pl", "*.wyborcza.pl", "*.wyborcza.biz", "*.wysokieobcasy.pl"],
+    contentScriptFile: data.url("script.js")
+});
