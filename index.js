233e9ff88a785c3ccb392083f701433edd6a154a
Krzysztof Głębowicz <krzysztof.glebowicz@it-pkp.pl>
2015-04-08 15:49:36 +0200

    Running script on DOM ready

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ var data = require("sdk/self").data;
 
 pageMod.PageMod({
     include: ["*.sport.pl", "*.gazeta.pl", "*.gazeta.biz", "*.tokfm.pl", "*.gazetapraca.pl", "*.moto.pl", "*.wyborcza.pl", "*.wyborcza.biz", "*.wysokieobcasy.pl", "*.polygamia.pl"],
-    contentScriptFile: data.url("script.js")
+    contentScriptFile: data.url("script.js"),
+	contentScriptWhen: "ready"
 });
